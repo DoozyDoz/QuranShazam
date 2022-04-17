68909ab8f0a985c8e92c6d17d2a5aff2dff0490f
Muhammad <mhdsemps@gmail.com>
2022-04-17 10:38:30 +0300

    base class

---
+++ app/src/main/java/com/kh69/quranshazam/extensions/Context.kt
@@ -0,0 +1,59 @@
+package com.kh69.quranshazam.extensions
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.kh69.quranshazam.helpers.*
+
+fun Context.hasPermission(permId: Int) = ContextCompat.checkSelfPermission(this, getPermissionString(permId)) == PackageManager.PERMISSION_GRANTED
+
+fun Context.getPermissionString(id: Int) = when (id) {
+    PERMISSION_READ_STORAGE -> Manifest.permission.READ_EXTERNAL_STORAGE
+    PERMISSION_WRITE_STORAGE -> Manifest.permission.WRITE_EXTERNAL_STORAGE
+    PERMISSION_CAMERA -> Manifest.permission.CAMERA
+    PERMISSION_RECORD_AUDIO -> Manifest.permission.RECORD_AUDIO
+    PERMISSION_READ_CONTACTS -> Manifest.permission.READ_CONTACTS
+    PERMISSION_WRITE_CONTACTS -> Manifest.permission.WRITE_CONTACTS
+    PERMISSION_READ_CALENDAR -> Manifest.permission.READ_CALENDAR
+    PERMISSION_WRITE_CALENDAR -> Manifest.permission.WRITE_CALENDAR
+    PERMISSION_CALL_PHONE -> Manifest.permission.CALL_PHONE
+    PERMISSION_READ_CALL_LOG -> Manifest.permission.READ_CALL_LOG
+    PERMISSION_WRITE_CALL_LOG -> Manifest.permission.WRITE_CALL_LOG
+    PERMISSION_GET_ACCOUNTS -> Manifest.permission.GET_ACCOUNTS
+    PERMISSION_READ_SMS -> Manifest.permission.READ_SMS
+    PERMISSION_SEND_SMS -> Manifest.permission.SEND_SMS
+    PERMISSION_READ_PHONE_STATE -> Manifest.permission.READ_PHONE_STATE
+    else -> ""
+}
+
+fun Context.toast(id: Int, length: Int = Toast.LENGTH_SHORT) {
+    toast(getString(id), length)
+}
+
+private fun doToast(context: Context, message: String, length: Int) {
+    if (context is Activity) {
+        if (!context.isFinishing && !context.isDestroyed) {
+            Toast.makeText(context, message, length).show()
+        }
+    } else {
+        Toast.makeText(context, message, length).show()
+    }
+}
+
+fun Context.toast(msg: String, length: Int = Toast.LENGTH_SHORT) {
+    try {
+        if (isOnMainThread()) {
+            doToast(this, msg, length)
+        } else {
+            Handler(Looper.getMainLooper()).post {
+                doToast(this, msg, length)
+            }
+        }
+    } catch (e: Exception) {
+    }
+}
